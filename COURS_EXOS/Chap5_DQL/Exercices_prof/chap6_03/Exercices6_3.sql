719a14bb960f7087671ce6adf770f0a6001a201b
Aurélien Savart <98811386+AurelienSavart@users.noreply.github.com>
2022-04-27 11:25:12 +0200

    Update and rename Exercices6_3.md to Exercices6_3.sql

--- COURS_EXOS/Chap5_DQL/Exercices_prof/chap6_03/Exercices6_3.sql
+++ COURS_EXOS/Chap5_DQL/Exercices_prof/chap6_03/Exercices6_3.sql
@@ -1,3 +1,3 @@
-!Modification de la table pilots pour ajouter la notion de lead pl : 
+/* Modification de la table pilots pour ajouter la notion de lead pl : */
 
 ALTER TABLE `pilots` ADD COLUMN `lead_pl` VARCHAR(6), ADD CONSTRAINT `fk_lead_pl` FOREIGN KEY(`lead_pl`) REFERENCES `pilots`(`certificate`) 
