214927a4ae2d66249d4e6acedcdade6e7b11a5c4
Aurélien Savart <98811386+AurelienSavart@users.noreply.github.com>
2022-04-27 11:40:08 +0200

    Update Exercices6_3.sql

--- COURS_EXOS/Chap5_DQL/Exercices_prof/chap6_03/Exercices6_3.sql
+++ COURS_EXOS/Chap5_DQL/Exercices_prof/chap6_03/Exercices6_3.sql
@@ -1,3 +1,7 @@
 /* Modification de la table pilots pour ajouter la notion de lead pl : */
 
 ALTER TABLE `pilots` ADD COLUMN `lead_pl` VARCHAR(6), ADD CONSTRAINT `fk_lead_pl` FOREIGN KEY(`lead_pl`) REFERENCES `pilots`(`certificate`) 
+
+/* Ajout des leads pilots */
+UPDATE pilots SET lead_pl = (SELECT ct FROM (SELECT certificate as ct FROM pilots WHERE name = 'Pierre') as p2) WHERE name IN ('Alan', 'Tom', 'Yi'); 
+UPDATE pilots SET lead_pl = (SELECT ct FROM (SELECT certificate as ct FROM pilots WHERE name = 'John')as p2) WHERE name IN ('Sophie', 'Albert', 'Yan'); 
